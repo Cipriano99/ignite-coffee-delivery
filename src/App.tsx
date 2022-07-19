194aa1c446bb8474beeffef3843976252b231ad3
Cipriano99 <samuelcipriano99@gmail.com>
2022-07-19 19:47:58 -0300

    add - Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/theme/default"
+import { ThemeProvider } from 'styled-components'
+import { Navbar } from './components/Navbar'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Navbar />
       <GlobalStyle />
     </ThemeProvider>
   )
