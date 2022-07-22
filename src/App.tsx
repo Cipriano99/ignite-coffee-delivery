8d6b14b81e5476b8b99b3651b9fa7e7a3018fb36
cipriano99 <samuelcipriano99@gmail.com>
2022-07-22 00:20:01 -0300

    add - React Router - DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Navbar } from './components/Navbar'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
