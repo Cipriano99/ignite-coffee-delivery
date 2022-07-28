a8086b3dfb8b2c63437b295f002e4bc565cb0c3e
cipriano99 <samuelcipriano99@gmail.com>
2022-07-27 23:20:30 -0300

    feat: add react toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { ToastContainer } from 'react-toastify'
+
+import { Router } from './Router'
 import { Navbar } from './components/Navbar'
 import { CartCoffeesProvider } from './contexts/CartCoffeesContext'
-import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const App = () => {
   return (
@@ -16,6 +20,16 @@ export const App = () => {
         </BrowserRouter>
       </CartCoffeesProvider>
       <GlobalStyle />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </ThemeProvider>
   )
 }
