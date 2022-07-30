c29531e494bf5a36340c3c3184dc91d9de29511c
cipriano99 <samuelcipriano99@gmail.com>
2022-07-30 16:52:10 -0300

    feat(formErrors): display errors with toast

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -6,13 +6,14 @@ import { FormDelivery } from './FormDelivery'
 import { Order } from './OrderDetails'
 
 import { CartContainer } from './styles'
+import { toast } from 'react-toastify'
 
 const formCartValidationSchema = zod.object({
-  cep: zod.string().min(8, 'Informe o cep corretamente'),
+  cep: zod.string().min(8, 'Informe o CEP corretamente'),
   bairro: zod.string().min(1, 'Informe o seu bairro'),
   cidade: zod.string().min(1, 'Informe a sua cidade'),
   complemento: zod.string(),
-  numero: zod.string().min(1, 'Informe a identificação'),
+  numero: zod.string().min(1, 'Informe a identificação da entrega'),
   rua: zod.string().min(1, 'Informe a sua rua'),
   uf: zod
     .string()
@@ -21,13 +22,21 @@ const formCartValidationSchema = zod.object({
 })
 
 export const Cart = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: zodResolver(formCartValidationSchema),
   })
 
   const handleFinishOrder = (data: any) => {
     console.log(data)
   }
+
+  Object.values(errors)
+    .map((error) => error)
+    .map((error) => toast.warn(`${error?.message}`))
 
   return (
     <CartContainer onSubmit={handleSubmit(handleFinishOrder)}>
